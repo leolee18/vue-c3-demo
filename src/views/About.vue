acb39e299ccaa9d4a4a249857f4059b550aa80b1
leolee18 <wwwliyuxi@163.com>
2019-06-15 10:39:57 +0800

    文字超出省略、文字两端对齐

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+		<div class="abo-ccsl">枯士大夫士大夫士大夫士大夫士大夫士大夫士大夫士大夫士大夫</div>
+		<div class="abo-lddq">枯士大</div>
   </div>
 </template>
 
@@ -19,3 +21,22 @@
 		
 	}
 </script>
+
+
+<style scoped>
+	.abo-ccsl{
+		position:relative;
+		width:200px;
+		overflow: hidden;
+		text-overflow: ellipsis;
+		white-space:nowrap;
+		background-color: #42B983;
+	}
+	.abo-lddq{
+		position:relative;
+		width:200px;
+		border: 1px solid red;
+    text-align: justify;
+    text-align-last:justify
+	}
+</style>
